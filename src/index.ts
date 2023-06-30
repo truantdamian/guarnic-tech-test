d04c79154e47621b8c907b7df72d3eea28e9ebd4
truantd <truantdamian@gmail.com>
2023-06-30 00:23:18 -0300

    change Index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 import { CarInsurance } from "./CarInsurance";
-import { Product } from "./Product";
+import { IProducts } from "./IProducts";
+import { createProduct } from "./ProductCreator";
 
 const productsAtDayZero = [
-  new Product("Medium Coverage", 10, 20),
-  new Product("Full Coverage", 2, 0),
-  new Product("Low Coverage", 5, 7),
-  new Product("Mega Coverage", 0, 80),
-  new Product("Mega Coverage", -1, 80),
-  new Product("Special Full Coverage", 15, 20),
-  new Product("Special Full Coverage", 10, 49),
-  new Product("Special Full Coverage", 5, 49),
-  new Product("Super Sale", 3, 6),
+  createProduct("Medium Coverage", 10, 20),
+  createProduct("Full Coverage", 2, 0),
+  createProduct("Low Coverage", 5, 7),
+  createProduct("Mega Coverage", 0, 80),
+  createProduct("Mega Coverage", -1, 80),
+  createProduct("Special Full Coverage", 15, 20),
+  createProduct("Special Full Coverage", 10, 49),
+  createProduct("Special Full Coverage", 5, 49),
+  createProduct("Super Sale", 3, 6),
 ];
 
 const carInsurance = new CarInsurance(productsAtDayZero);
-const productPrinter = function (product: Product) {
+
+const productPrinter = function (product: IProducts) {
   console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
 };
+
+console.log(`Day 0`);
+productsAtDayZero.forEach(productPrinter);
+console.log("-------------------------------------");
+console.log("\n");
 
 for (let i = 1; i <= 30; i += 1) {
   console.log(`Day ${i}`);
   console.log("name, sellIn, price");
   carInsurance.updatePrice().forEach(productPrinter);
-  console.log("");
+  console.log("-------------------------------------");
+  console.log("\n");
 }
